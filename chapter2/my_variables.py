f36d1d8d26a050a20fc090dcac556ae7a434047d
marvek16 <marvelk16@gmail.com>
2025-02-24 13:59:49 +0200

    first commit

---
+++ chapter2/my_variables.py
@@ -0,0 +1,51 @@
+# name = "Marvelous"
+# surname = " Favour"
+# print(name + surname)
+# full_name = name + surname 
+# print(full_name)
+
+
+# sentence = "i love chocolate cake!"
+# print(sentence.title())
+
+# name = "Ada Lovelace"
+# print(name.upper())
+# print(name.lower())
+
+# fname = "kelly"
+# lname = "keet"
+# fullname = f"{fname} {lname}"
+# print(f"Hello, my bestfriend's name is,  {fullname.title()}!")
+
+
+# print("\tHi.\nMy name is Marvelous Favour.\nI am 19years old.\nI currently learning python.")
+
+# num1 = 3
+# num2 = 4
+# num3 = 7
+# num4 = 9
+
+# numbers = num1 * num2 + (num3 - num4)
+# print(numbers)
+
+# one, two, three = 1, 2, 3
+# print(two)
+
+# str1, str2, str3 = "Hi", "Hello", "Bonjour"
+# print(str2)
+
+# NEVER_CHANGE_VALUE = "ALWAYS THE SAME"
+
+# # NEVER_CHANGE_VALUE = 'HI'
+
+# print(NEVER_CHANGE_VALUE)
+
+num1,num2,num3, num4 = 3, 4, 7, 9
+print(num2)
+
+numbers = num1 * num2 + (num3 - num4)
+print(numbers)
+
+import this
+# The Zen of Python, by Tim Peters
+# Beautiful is better than ugly.
