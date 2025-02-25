dd7259fd2da5eac942af5d2dce9c8b580de63f6e
marvek16 <marvelk16@gmail.com>
2025-02-25 15:10:13 +0200

    commit

---
+++ chapter4/chap4.py
@@ -0,0 +1,28 @@
+# favourite_books_series = ['the plated prisoner', 'the empyrean', 'throne of glass', 'ACOTAR']
+
+# for book_series in favourite_books_series:
+#     book_title = book_series.title()
+#     print(f"This is one of my favourite books: {book_title}")
+    
+# for book_series in favourite_books_series:
+#     print(f"This is one of my favourite books: {book_series.title()}")
+
+# for number in range(1, 10):
+#     print(number)
+    
+# numbers = range (2, 20)
+# empty_list = []
+# print(empty_list)
+
+# for number in numbers: 
+#     empty_list.append(number)  
+#     # print(number) 
+      
+# print(empty_list)   
+    
+    
+my_list = ['item1', 'item2', 'item3', 'item4']
+my_tuple = ("one", "two", "three")
+
+my_list.reverse()
+print(my_list)
