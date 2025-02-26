f80497082dad927c99c7b037919836ed9e704b8d
marvek16 <marvelousk1603f@gmail.com>
2025-02-26 09:52:18 +0200

    first commit

---
+++ chapter5/chap5.py
@@ -0,0 +1,13 @@
+my_lists = ["chocolate cake", "cinnamon rolls", "reading fantasy", "sour sweets"]
+my_fav_car = "audi"
+
+for list in my_lists:
+    if list == "reading fantasy":
+        print("Fantasy is amazing!")
+else:
+    print("those sound tasty!")
+    
+if my_fav_car == "Audi":
+    print("You have great taste!")
+else:
+    print("Meh")
