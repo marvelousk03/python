62b285ba15389c4e5c34d3b57095f10cc6545da5
marvek16 <marvelousk1603f@gmail.com>
2025-02-26 11:50:32 +0200

    commit

--- chapter5/chap5.py
+++ chapter5/chap5.py
@@ -1,13 +1,30 @@
-my_lists = ["chocolate cake", "cinnamon rolls", "reading fantasy", "sour sweets"]
-my_fav_car = "audi"
+# my_lists = ["chocolate cake", "cinnamon rolls", "reading fantasy", "sour sweets"]
+# my_fav_car = "audi"
+# tester = True
+# things = ["ball", "t-shirt", "jacket", "phone"]
 
-for list in my_lists:
-    if list == "reading fantasy":
-        print("Fantasy is amazing!")
-else:
-    print("those sound tasty!")
+# for list in my_lists:
+#     if list == "reading fantasy":
+#         print("Fantasy is amazing!")
+# else:
+#     print("those sound tasty!")
     
-if my_fav_car == "Audi":
-    print("You have great taste!")
-else:
-    print("Meh")
+# if my_fav_car == "Audi":
+#     print("You have great taste!")
+# else:
+#     print("Meh")
+    
+# if False == False or False == tester:  #false == false || false; javascript example
+#     print("All conditions are met")    
+# else:
+#     print("Conditions are not 100%")
+    
+# if "coffee" in things:
+#     print("That is in there")    
+# else:
+#     print("That is not in there")
+    
+# for value in my_lists:
+#     if "reading fantasy" and "chocolate cake":
+#         print("That is going to be an amazing reading experience")
+
