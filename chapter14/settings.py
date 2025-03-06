96aa569dab04bfc37340541c00c57e8d47c1c267
marvek16 <marvelousk1603f@gmail.com>
2025-03-06 09:47:22 +0200

    commit

--- chapter14/settings.py
+++ chapter14/settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """Initialize the game's static settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 400
         self.bg_color = (230, 230, 230)
 
         # Ship settings
@@ -14,11 +14,11 @@ class Settings:
         # Bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullet_color = (0, 0, 0)
+        self.bullets_allowed = 100
 
         # Alien settings
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 5
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
@@ -29,8 +29,8 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 2.5
+        self.ship_speed = 2
+        self.bullet_speed = 3
         self.alien_speed = 1.0
 
         # fleet_direction of 1 represents right; -1 represents left.
