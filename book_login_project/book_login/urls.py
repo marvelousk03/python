8aaee6d484bd7a70665f0c2f225896d8b5e2f9b9
marvek16 <marvelousk1603f@gmail.com>
2025-03-20 11:40:52 +0200

    commit

--- book_login_project/book_login/urls.py
+++ book_login_project/book_login/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from books.views import home
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
 ]
